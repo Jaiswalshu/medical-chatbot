a31b1e0b15d72da2cab49bebe20be793e03629aa
Jaiswalshu <133133679+Jaiswalshu@users.noreply.github.com>
2024-09-01 21:51:45 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,374 @@
+var data= {
+    chatinit:{
+        title: ["Hello, <span class='emoji'> &#128075;I can help you if you’re not feeling well. </span>","I get smarter the more you tell me, so I’m going to ask a few questions that will help me help you. "],
+        options: ['continue','notcontinue']
+    },
+
+    
+    continue: {
+        title:
+    ["<b>My_hurts--></b>abdomen-back-chest-ear-pelvis-tooth-rectum-skin-Extremities-leg-chronic pain"
+    ,"<b>I feel:--></b>Chills-Fever-Paresthesia (numbness, tingling, electric tweaks)-Dizzy-Dizzy(about to black out)-Dizzy(with the room spinning around me)-My mouth is dry-Nauseated-Sick-like I have the flu-like I have to vomit-Short of breath-Sleepy-Sweaty-Thirsty-Tired-Weak"
+    ,"<b>I can not:--></b>Breathe normally-Hear normally:-losing hearing-sounds are too loud-ringing or hissing in my ears-Move one side(arm and/or leg)- Defecate normally-Urinate normally-Remember normally-See properly-(Blindness,blurred vision,double vision)-Sleep normally-Smell things normally-Speak normally-Excrete solid feces-Stop scratching-Stop sweating-Swallow normally-Taste properly-Walk normally-Write normally"   
+    ],
+        options:['hurts','feels','cannot',],
+        url : {
+            
+        }
+
+        
+    },
+
+     notcontinue: {
+        title:["Please close the chat from left side button"],
+        options:[],
+        url : {
+            
+        }
+
+        
+    },
+        
+
+    hurts: {
+        title:["Thanks for your response please answer these questions",],
+        options:['abdomen','back','chest','ear','head','pelvic','tooth',,'skin','leg','Chronic',],
+        url : {
+            
+        }
+
+        
+    },
+
+
+
+    abdomen: {
+        title:["Thanks for your response please Go throw see more for knowning about these these sympotos","Abdominal pain, also known as a stomach ache, is a symptom associated with both non-serious and serious medical issues. Since the abdomen contains most of the body's vital organs, it can be an indicator of a wide variety of diseases. Given that, approaching the examination of a person and planning of a differential diagnosis is extremely important"],
+        options:[],
+        url : {
+            more:"https://en.wikipedia.org/wiki/Abdominal_pain",
+            link:["https://en.wikipedia.org/wiki/Abdominal_pain","https://en.wikipedia.org/wiki/Abdominal_pain","https://en.wikipedia.org/wiki/Abdominal_pain","https://en.wikipedia.org/wiki/Abdominal_pain","https://en.wikipedia.org/wiki/Abdominal_pain"]
+        }
+
+        
+    },
+
+    back: {
+        title:["Thanks for your response please Go throw see more for knowning about these these sympotos","Back pain (Latin: dorsalgia) is pain felt in the back. It may be classified as neck pain (cervical), middle back pain (thoracic), lower back pain (lumbar) or coccydynia (tailbone or sacral pain) based on the segment affected.[1] The lumbar area is the most common area affected.[2] An episode of back pain may be acute, subacute or chronic depending on the duration. The pain may be characterized as a dull ache, shooting or piercing pain or a burning sensation. Discomfort can radiate to the arms and hands as well as the legs or feet,[3] and may include numbness[1] or weakness in the legs and arms."],
+        options:[],
+        url : {
+            more:"https://en.wikipedia.org/wiki/Back_pain",
+            link:["https://en.wikipedia.org/wiki/Back_pain","https://en.wikipedia.org/wiki/Back_pain","https://en.wikipedia.org/wiki/Back_pain","https://en.wikipedia.org/wiki/Back_pain","https://en.wikipedia.org/wiki/Back_pain"]
+        }
+
+        
+    },
+    chest: {
+        title:["Thanks for your response please Go throw see more for knowning about these these sympotos","Chest pain is pain or discomfort in the chest, typically the front of the chest.[1] It may be described as sharp, dull, pressure, heaviness or squeezing.[3] Associated symptoms may include pain in the shoulder, arm, upper abdomen, or jaw, along with nausea, sweating, or shortness of breath.[1][3] It can be divided into heart-related and non-heart-related pain.[1][2] Pain due to insufficient blood flow to the heart is also called angina pectoris.[5] Those with diabetes or the elderly may have less clear symptoms.[3]"],
+        options:[],
+        url : {
+            more:"https://en.wikipedia.org/wiki/Chest_pain",
+            link:["https://en.wikipedia.org/wiki/Chest_pain","https://en.wikipedia.org/wiki/Chest_pain","https://en.wikipedia.org/wiki/Chest_pain","https://en.wikipedia.org/wiki/Chest_pain","https://en.wikipedia.org/wiki/Chest_pain"]
+        }
+
+        
+    },
+
+    ear: {
+        title:["Thanks for your response please Go throw see more for knowning about these these sympotos","Ear pain, also known as earache or otalgia, is pain in the ear.[1][2] Primary ear pain is pain that originates from the ear. Secondary ear pain is a type of referred pain, meaning that the source of the pain differs from the location where the pain is felt."],
+        options:[],
+        url : {
+            more:"https://en.wikipedia.org/wiki/Ear_pain",
+            link:["https://en.wikipedia.org/wiki/Ear_pain","https://en.wikipedia.org/wiki/Ear_pain","https://en.wikipedia.org/wiki/Ear_pain","https://en.wikipedia.org/wiki/Ear_pain","https://en.wikipedia.org/wiki/Ear_pain"]
+        }
+
+        
+    },
+
+
+    head: {
+        title:["Thanks for your response please Go throw see more for knowning about these these sympotos","Headache, also known as cephalalgia, is the symptom of pain in the face, head, or neck. It can occur as a migraine, tension-type headache, or cluster headache.[1][2] There is an increased risk of depression in those with severe headaches.[3]"],
+        options:[],
+        url : {
+            more:"https://en.wikipedia.org/wiki/Headache",
+            link:["https://en.wikipedia.org/wiki/Headache","https://en.wikipedia.org/wiki/Headache","https://en.wikipedia.org/wiki/Headache","https://en.wikipedia.org/wiki/Headache","https://en.wikipedia.org/wiki/Headache"]
+        }
+
+        
+    },
+
+
+    pelvic: {
+        title:["Thanks for your response please Go throw see more for knowning about these these sympotos","Pelvic pain is pain in the area of the pelvis. Acute pain is more common than chronic pain.[2] If the pain lasts for more than six months, it is deemed to be chronic pelvic pain.[3][4] It can affect both the male and female pelvis."],
+        options:[],
+        url : {
+            more:"https://en.wikipedia.org/wiki/Pelvic_pain",
+            link:["https://en.wikipedia.org/wiki/Pelvic_pain","https://en.wikipedia.org/wiki/Pelvic_pain","https://en.wikipedia.org/wiki/Pelvic_pain","https://en.wikipedia.org/wiki/Pelvic_pain","https://en.wikipedia.org/wiki/Pelvic_pain"]
+        }
+
+        
+    },
+
+
+    tooth: {
+        title:["Thanks for your response please Go throw see more for knowning about these these sympotos","Toothache, also known as dental pain or tooth pain,[3] is pain in the teeth or their supporting structures, caused by dental diseases or pain referred to the teeth by non-dental diseases. When severe it may impact sleep, eating, and other daily activities."],
+        options:[],
+        url : {
+            more:"https://en.wikipedia.org/wiki/Toothache",
+            link:["https://en.wikipedia.org/wiki/Toothache","https://en.wikipedia.org/wiki/Toothache","https://en.wikipedia.org/wiki/Toothache","https://en.wikipedia.org/wiki/Toothache","https://en.wikipedia.org/wiki/Toothache"]
+        }
+
+        
+    },
+
+
+    skin: {
+        title:["Thanks for your response please Go throw see more for knowning about these these sympotos","A dermatome is an area of skin that is mainly supplied by afferent nerve fibres from the dorsal root of any given spinal nerve.[1][2] There are 8 cervical nerves (C1 being an exception with no dermatome), 12 thoracic nerves, 5 lumbar nerves and 5 sacral nerves. Each of these nerves relays sensation (including pain) from a particular region of skin to the brain."],
+        options:[],
+        url : {
+            more:"https://en.wikipedia.org/wiki/Dermatome_(anatomy)",
+            link:["https://en.wikipedia.org/wiki/Dermatome_(anatomy)","https://en.wikipedia.org/wiki/Dermatome_(anatomy)","https://en.wikipedia.org/wiki/Dermatome_(anatomy)","https://en.wikipedia.org/wiki/Dermatome_(anatomy)","https://en.wikipedia.org/wiki/Dermatome_(anatomy)"]
+        }
+
+        
+    },
+
+
+
+    leg : {
+        title:["Thanks for your response please Go throw see more for knowning about these these sympotos","Patellofemoral pain syndrome (PFPS; not to be confused with jumper's knee) is knee pain as a result of problems between the kneecap and the femur.[4] The pain is generally in the front of the knee and comes on gradually.[2][4] Pain may worsen with sitting, excessive use, or climbing and descending stairs.["],
+        options:[],
+        url : {
+            more:"https://en.wikipedia.org/wiki/Patellofemoral_pain_syndrome",
+            link:["https://en.wikipedia.org/wiki/Patellofemoral_pain_syndrome","https://en.wikipedia.org/wiki/Patellofemoral_pain_syndrome","https://en.wikipedia.org/wiki/Patellofemoral_pain_syndrome","https://en.wikipedia.org/wiki/Patellofemoral_pain_syndrome","https://en.wikipedia.org/wiki/Patellofemoral_pain_syndrome"]
+        }
+
+        
+    },
+
+    Chronic: {
+        title:["Thanks for your response please Go throw see more for knowning about these these sympotos","Chronic pain or chronic pain syndrome is a type of pain that is also known by other titles such as gradual burning pain, electrical pain, throbbing pain, and nauseating pain. This type of pain is sometimes confused with acute pain[a] and can last from three months to several years; Various diagnostic manuals such as DSM-5 and ICD-11 have proposed several definitions of chronic pain, but the accepted definition is that it is pain that lasts longer than the expected period of recovery"],
+        options:[],
+        url : {
+            more:"https://en.wikipedia.org/wiki/Chronic_pain",
+            link:["https://en.wikipedia.org/wiki/Chronic_pain","https://en.wikipedia.org/wiki/Chronic_pain","https://en.wikipedia.org/wiki/Chronic_pain","https://en.wikipedia.org/wiki/Chronic_pain","https://en.wikipedia.org/wiki/Chronic_pain"]
+        }
+
+        
+    },
+
+    
+
+
+  
+    feels: {
+        title:["Thanks for your response please answer these questions",],
+        options:['Chills','Fever','Paresthesia','Light headed','Dizzy','-My mouth is dry','Nauseated','Sick', 'like I have the flu','like I have to vomit','Short of breath','Sleepy-Sweaty','Thirsty','Tired-Weak',],
+        url : {
+            
+        }
+
+        
+    },
+
+    // Fever: {
+    //     title:["Thanks for your response please Go throw see more for knowning about these these sympotos","Chronic pain or chronic pain syndrome is a type of pain that is also known by other titles such as gradual burning pain, electrical pain, throbbing pain, and nauseating pain. This type of pain is sometimes confused with acute pain[a] and can last from three months to several years; Various diagnostic manuals such as DSM-5 and ICD-11 have proposed several definitions of chronic pain, but the accepted definition is that it is pain that lasts longer than the expected period of recovery"],
+    //     options:[],
+    //     url : {
+    //         // more:"https://en.wikipedia.org/wiki/Chronic_pain",
+    //         // link:["https://en.wikipedia.org/wiki/Chronic_pain","https://en.wikipedia.org/wiki/Chronic_pain","https://en.wikipedia.org/wiki/Chronic_pain","https://en.wikipedia.org/wiki/Chronic_pain","https://en.wikipedia.org/wiki/Chronic_pain"]
+    //     }
+
+        
+    // },
+
+    // cannot: {
+    //     title:["Thanks for your response please Go throw see more for knowning about these these sympotos",],
+    //     options:['Breathe','Hear normally','losing hearing','sounds are too loud','ringing or hissing in my ears','Move one side(arm and/or leg)',' Defecate normally','Urinate normally','Remember normally','See properly',],
+    //     url : {
+            
+    //     }
+
+        
+    // },
+    // Breathe: {
+    //     title:["Thanks for your response please Go throw see more for knowning about these these sympotos","Chronic pain or chronic pain syndrome is a type of pain that is also known by other titles such as gradual burning pain, electrical pain, throbbing pain, and nauseating pain. This type of pain is sometimes confused with acute pain[a] and can last from three months to several years; Various diagnostic manuals such as DSM-5 and ICD-11 have proposed several definitions of chronic pain, but the accepted definition is that it is pain that lasts longer than the expected period of recovery"],
+    //     options:[],
+    //     url : {
+    //         // more:"https://en.wikipedia.org/wiki/Chronic_pain",
+    //         // link:["https://en.wikipedia.org/wiki/Chronic_pain","https://en.wikipedia.org/wiki/Chronic_pain","https://en.wikipedia.org/wiki/Chronic_pain","https://en.wikipedia.org/wiki/Chronic_pain","https://en.wikipedia.org/wiki/Chronic_pain"]
+    //     }
+    // },
+
+    cannot: {
+        title:["Thanks for your response please answer these questions",],
+        options:['hello','hi',],
+        url : {
+            
+        }
+
+        
+    },
+    hello: {
+        title:["Thanks for your response please Go throw see more for knowning about these these sympotos","Chronic pain or chronic pain syndrome is a type of pain that is also known by other titles such as gradual burning pain, electrical pain, throbbing pain, and nauseating pain. This type of pain is sometimes confused with acute pain[a] and can last from three months to several years; Various diagnostic manuals such as DSM-5 and ICD-11 have proposed several definitions of chronic pain, but the accepted definition is that it is pain that lasts longer than the expected period of recovery"],
+        options:[],
+        url : {
+            more:"https://en.wikipedia.org/wiki/Chronic_pain",
+            link:["https://en.wikipedia.org/wiki/Chronic_pain","https://en.wikipedia.org/wiki/Chronic_pain","https://en.wikipedia.org/wiki/Chronic_pain","https://en.wikipedia.org/wiki/Chronic_pain","https://en.wikipedia.org/wiki/Chronic_pain"]
+        }
+
+        
+    },
+
+    hi: {
+        title:["Thanks for your response please Go throw see more for knowning about these these sympotos","Chronic pain or chronic pain syndrome is a type of pain that is also known by other titles such as gradual burning pain, electrical pain, throbbing pain, and nauseating pain. This type of pain is sometimes confused with acute pain[a] and can last from three months to several years; Various diagnostic manuals such as DSM-5 and ICD-11 have proposed several definitions of chronic pain, but the accepted definition is that it is pain that lasts longer than the expected period of recovery"],
+        options:[],
+        url : {
+            more:"https://en.wikipedia.org/wiki/Chronic_pain",
+            link:["https://en.wikipedia.org/wiki/Chronic_pain","https://en.wikipedia.org/wiki/Chronic_pain","https://en.wikipedia.org/wiki/Chronic_pain","https://en.wikipedia.org/wiki/Chronic_pain","https://en.wikipedia.org/wiki/Chronic_pain"]
+        }
+
+        
+    },
+    
+    
+    
+
+
+
+}
+
+document.getElementById("init").addEventListener("click",showChatBot);
+var cbot= document.getElementById("chat-box");
+
+var len1= data.chatinit.title.length;
+
+function showChatBot(){
+    console.log(this.innerText); 
+    if(this.innerText=='START CHAT'){
+        document.getElementById('test').style.display='block';
+        document.getElementById('init').innerText='CLOSE CHAT';
+        initChat();
+    }
+    else{
+        location.reload();
+    }
+}
+
+function initChat(){
+    j=0;
+    cbot.innerHTML='';
+    for(var i=0;i<len1;i++){
+        setTimeout(handleChat,(i*500));
+    }
+    setTimeout(function(){
+        showOptions(data.chatinit.options)
+    },((len1+1)*500))
+}
+
+var j=0;
+function handleChat(){
+    console.log(j);
+    var elm= document.createElement("p");
+    elm.innerHTML= data.chatinit.title[j];
+    elm.setAttribute("class","msg");
+    cbot.appendChild(elm);
+    j++;
+    handleScroll();
+}
+
+function showOptions(options){
+    for(var i=0;i<options.length;i++){
+        var opt= document.createElement("span");
+        var inp= '<div>'+options[i]+'</div>';
+        opt.innerHTML=inp;
+        opt.setAttribute("class","opt");
+        opt.addEventListener("click", handleOpt);
+        cbot.appendChild(opt);
+        handleScroll();
+    }
+}
+
+function handleOpt(){
+    console.log(this);
+    var str= this.innerText;
+    var textArr= str.split(" ");
+    var findText= textArr[0];
+    
+    document.querySelectorAll(".opt").forEach(el=>{
+        el.remove();
+    })
+    var elm= document.createElement("p");
+    elm.setAttribute("class","test");
+    var sp= '<span class="rep">'+this.innerText+'</span>';
+    elm.innerHTML= sp;
+    cbot.appendChild(elm);
+
+    console.log(findText.toLowerCase());
+    var tempObj= data[findText.toLowerCase()];
+    handleResults(tempObj.title,tempObj.options,tempObj.url);
+}
+
+function handleDelay(title){
+    var elm= document.createElement("p");
+        elm.innerHTML= title;
+        elm.setAttribute("class","msg");
+        cbot.appendChild(elm);
+}
+
+
+function handleResults(title,options,url){
+    for(let i=0;i<title.length;i++){
+        setTimeout(function(){
+            handleDelay(title[i]);
+        },i*500)
+        
+    }
+
+    const isObjectEmpty= (url)=>{
+        return JSON.stringify(url)=== "{}";
+    }
+
+    if(isObjectEmpty(url)==true){
+        console.log("having more options");
+        setTimeout(function(){
+            showOptions(options);
+        },title.length*500)
+        
+    }
+    else{
+        console.log("end result");
+        setTimeout(function(){
+            handleOptions(options,url);
+        },title.length*500)
+        
+    }
+}
+
+function handleOptions(options,url){
+    for(var i=0;i<options.length;i++){
+        var opt= document.createElement("span");
+        var inp= '<a class="m-link" href="'+url.link[i]+'">'+options[i]+'</a>';
+        opt.innerHTML=inp;
+        opt.setAttribute("class","opt");
+        cbot.appendChild(opt);
+    }
+    var opt= document.createElement("span");
+    var inp= '<a class="m-link" href="'+url.more+'">'+'See more</a>';
+
+    const isObjectEmpty= (url)=>{
+        return JSON.stringify(url)=== "{}";
+    }
+
+    console.log(isObjectEmpty(url));
+    console.log(url);
+    opt.innerHTML=inp;
+    opt.setAttribute("class","opt link");
+    cbot.appendChild(opt);
+    handleScroll();
+}
+
+function handleScroll(){
+    var elem= document.getElementById('chat-box');
+    elem.scrollTop= elem.scrollHeight;
+}
